82cf889ced1c5c72768aa74436950b9be9edacf8
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-10-11 14:36:15 -0400

    Update styling of hellow-swift example and make the Swift a bit more interesting

--- hello-swift/src/main/swift/Sources/helloswift/helloswift.swift
+++ hello-swift/src/main/swift/Sources/helloswift/helloswift.swift
@@ -2,7 +2,7 @@ import Android
 
 @_cdecl("Java_org_example_helloswift_MainActivity_stringFromSwift")
 public func MainActivity_stringFromSwift(env: UnsafeMutablePointer<JNIEnv?>, clazz: jclass) -> jstring {
-    let hello = "Hello from Swift"
+    let hello = ["Hello", "from", "Swift", "❤️"].joined(separator: " ")
     return hello.withCString { ptr in
     	env.pointee!.pointee.NewStringUTF(env, ptr)!
     }
