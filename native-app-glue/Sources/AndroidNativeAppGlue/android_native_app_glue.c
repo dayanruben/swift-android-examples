b92e5077ffe192d2412e95157b17693ee209b5e7
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-10-11 15:11:22 -0400

    Add license headers

--- native-app-glue/Sources/AndroidNativeAppGlue/android_native_app_glue.c
+++ native-app-glue/Sources/AndroidNativeAppGlue/android_native_app_glue.c
@@ -1,3 +1,14 @@
+//===-- android_native_app_glue.c - Native Android app glue -----*- C++ -*-===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 #include "android_native_app_glue.h"
 
 #include <jni.h>
