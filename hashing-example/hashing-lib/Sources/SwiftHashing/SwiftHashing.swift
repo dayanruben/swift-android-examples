494d2cce96c155d1646dcd9c222e2be0fec5f6d3
Mads Odgaard <mads.odgaard@frameo.com>
2025-10-15 14:46:50 +0200

    add hashing example app that uses swift-java jni mode

---
+++ hashing-example/hashing-lib/Sources/SwiftHashing/SwiftHashing.swift
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Crypto
+import FoundationEssentials
+
+public func hash(_ input: String) -> String {
+    SHA256.hash(data: Data(input.utf8)).description
+}
---
+++ hashing-example/hashing-lib/Tests/SwiftHashingTests/SwiftHashingTests.swift
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Testing
+@testable import SwiftHashing
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
